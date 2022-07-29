c8003b61c984f3984c3468ab677a618d42b65c9b
Thiago_Rocha <thiago_slipknot123@hotmail.com>
2022-07-28 21:09:57 -0300

    commit project email

---
+++ src/main/java/br/com/throchadev/demoemail/controller/EmailController.java
@@ -0,0 +1,23 @@
+package br.com.throchadev.demoemail.controller;
+
+import br.com.throchadev.demoemail.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/email")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping(value = "/send")
+    public String sendemail() {
+
+        emailService.sendMail("thiagoslipknot31@gmail.com", "Test Subject", "Test email");
+
+        return "email-sent";
+    }
+}
---
+++ src/main/java/br/com/throchadev/demoemail/service/EmailService.java
@@ -0,0 +1,24 @@
+package br.com.throchadev.demoemail.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendMail(String toEmail, String subject, String message) {
+
+        var mailMessage = new SimpleMailMessage();
+
+        mailMessage.setTo(toEmail);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        javaMailSender.send(mailMessage);
+    }
+}
